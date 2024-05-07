33a922023a5830930a60406533abe4875bff49bb
ritik457 <144312757+ritik457@users.noreply.github.com>
2024-05-07 19:26:44 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+const form = document.getElementById("form");
+const username = document.getElementById("userName");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const password2 = document.getElementById("confirmPassword");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  checkInputs();
+});
+
+function checkInputs() {
+  //Get the value the form field.
+  const usernameValue = username.value.trim(); //trim to delete blanc space.
+  const emailValue = email.value.trim();
+  const passwordValue = password.value.trim();
+  const password2Value = password2.value.trim();
+
+  if (usernameValue === "") {
+    setErrorInput(username, "UserName cannot be blanc.");
+  } else {
+    setSuccessInput(username);
+  }
+
+  //###################################
+  if (emailValue === "") {
+    setErrorInput(email, "Email cannot be blanc.");
+  } else {
+    validateEmail(emailValue) && setSuccessInput(email);
+  }
+
+  //###################################
+  if (passwordValue === "") {
+    setErrorInput(password, "Password connot be blanc.");
+  } else {
+    setSuccessInput(password) && validatePassword(passwordValue);
+  }
+
+  //###################################
+  if (password2Value === "") {
+    setErrorInput(password2, "Password connot be blanc.");
+  } else if (password2Value !== passwordValue) {
+    setErrorInput(password2, "Password dose not match.");
+  } else {
+    setSuccessInput(password2);
+  }
+}
+
+function setErrorInput(input, errorMessage) {
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  small.innerText = errorMessage;
+  formControl.className = "form__control error";
+}
+
+function setSuccessInput(input) {
+  const formControl = input.parentElement;
+  formControl.className = "form__control success";
+}
+
+function validateEmail(email) {
+  let regular_expressions = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return regular_expressions.test(String(email).toLocaleLowerCase());
+}
+
+function validatePassword(password) {
+  let regular_expressions = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/;
+  return regular_expressions.match(regular_expressions);
+}
